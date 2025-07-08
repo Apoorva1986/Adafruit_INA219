8e8bb83e2b2c6c692eb0fe09954af70e8f1e3849
Apoorva1986 <apoorva.alankar@gmail.com>
2025-07-08 19:23:34 +0530

    Update Adafruit_INA219.cpp

--- Adafruit_INA219.cpp
+++ Adafruit_INA219.cpp
@@ -484,3 +484,10 @@ void Adafruit_INA219::setCalibration_16V_400mA() {
  *          result is stored.
  */
 bool Adafruit_INA219::success() { return _success; }
+void Adafruit_INA219::setCalibration_16V_100mA() {
+  ina219_calValue = 10240;
+  ina219_currentDivider_mA = 25.0;
+  ina219_powerDivider_mW = 1.0;
+
+  wireWriteRegister(INA219_REG_CALIBRATION, ina219_calValue);
+}
